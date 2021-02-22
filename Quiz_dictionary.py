edf047e53f4a5248db452fd06c5f065cde333b5f
Harsimran Thethi <harsimran.thethi@gmail.com>
2021-02-22 17:42:58 +0530

    Sample

---
+++ Quiz_dictionary.py
@@ -0,0 +1,21 @@
+import random
+q1 = """The place of origin of an earthquake is called:
+a.Epicentre\nb.Seismal\nc.Focus\nd.Amphidromic Point"""
+q2 = """The capital of Columbia is:
+a.Bogota\nb.Olympus\nc.Rome\nd.Jakarta"""
+q3 = """The busiest  sea route is:
+a.The Mediterranean Red-Sea Route\nb.The South Atlantic Route\nc.The North Atlantic Route\nd.The Pacific Route"""
+score = 0
+questions = {q1:"c",q2:"a",q3:"c"} # Answer dictionary
+rand_ques = random.sample(list(questions),2) # To randomly select 2 questions
+for i in rand_ques:
+    print(i)
+    print()
+    answer = input("Enter your answer: ")
+    print()
+    if answer == questions[i]:
+        print("Well done! You get one point.")
+        score = score + 1    
+    else:
+        print("Sorry, wrong answer")
+print("Final score is:",score)
