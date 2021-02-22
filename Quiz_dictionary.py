c89560d44b630077af3832fa4ee1faca5ae8bb6f
Harsimran <harsimran.thethi@gmail.com>
2021-02-22 17:47:23 +0530

    Update Quiz_dictionary.py

--- Quiz_dictionary.py
+++ Quiz_dictionary.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 q1 = """The place of origin of an earthquake is called:
 a.Epicentre\nb.Seismal\nc.Focus\nd.Amphidromic Point"""
@@ -6,8 +5,10 @@ q2 = """The capital of Columbia is:
 a.Bogota\nb.Olympus\nc.Rome\nd.Jakarta"""
 q3 = """The busiest  sea route is:
 a.The Mediterranean Red-Sea Route\nb.The South Atlantic Route\nc.The North Atlantic Route\nd.The Pacific Route"""
+
+questions = {q1:"c",q2:"a",q3:"c"} # Question-Answer dictionary
+
 score = 0
-questions = {q1:"c",q2:"a",q3:"c"} # Answer dictionary
 rand_ques = random.sample(list(questions),2) # To randomly select 2 questions
 for i in rand_ques:
     print(i)
