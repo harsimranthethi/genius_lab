25f31b460c69ec0f248ce4f74924f58174b2ca92
Harsimran Thethi <harsimran.thethi@gmail.com>
2021-03-09 14:22:12 +0530

    Final

--- Quiz_gui.py
+++ Quiz_gui.py
@@ -29,26 +29,104 @@ answers_choice = [
 ]
 
 answers = [2,2,2,1,1,1,1,1,1,1]
-
+answers_choice_bonus = [1]
 user_answer = []
-
+user_answerb = []
 indexes = []
 def gen():
+    global user
     global indexes
-    while(len(indexes)<5):
+    while(len(indexes)<user):
         x = random.randint(0,9)
         if x in indexes:
             continue
         else:
             indexes.append(x)
 
-def bonques():
+def quit(): # Quit
+    root.destroy()
+
+
+
+def wrong():
+    bonimage.destroy()
+    bontext.destroy()
+    x1.destroy()
+    x2.destroy()
+    x3.destroy()
+    x4.destroy()
+    bonus_image = Label(root,background = "#ffffff")
+    bonus_image.pack(pady=(100,80))
+    bonus_ques = PhotoImage(file = "bear.gif")
+    bonus_image.configure(image = bonus_ques)
+    bonus_image.image = bonus_ques
+    labelresulttext_bonus = Label(root,font = ("Consolas",30),background = "#ffffff")
+    labelresulttext_bonus.pack()
+    labelresulttext_bonus.configure(text = "Wrong Answer\nYour score is:{}".format(score),foreground = "#E77471")
+    quitwrong = Button(root,text="Quit",fg = "blue",height = 2,width=7,background = "#ffffff",font = ("Comic sans MS",30),command = quit)
+    quitwrong.pack(pady = (15,30))
+
+def correct():
+    global score
+    bonimage.destroy()
+    bontext.destroy()
+    x1.destroy()
+    x2.destroy()
+    x3.destroy()
+    x4.destroy()
+    bonus_image = Label(root,background = "#ffffff")
+    bonus_image.pack(pady=(100,80))
+    bonus_ques = PhotoImage(file = "download.png")
+    bonus_image.configure(image = bonus_ques)
+    bonus_image.image = bonus_ques
+    labelresulttext_bonus = Label(root,font = ("Consolas",30),background = "#ffffff")
+    labelresulttext_bonus.pack()
+    score = score + 10
+    labelresulttext_bonus.configure(text = "Correct!\nYour score is:{}\nThanks for playing!".format(score),foreground = "#E77471")
+    quitcorrect = Button(root,text="Quit",fg = "blue",height = 2,width=7,background = "#ffffff",font = ("Comic sans MS",30),command = quit)
+    quitcorrect.pack(pady = (15,30))
+
+
+
+def bonround(): #Bonus question
+    global bontext
+    global bonimage
+    global x1,x2,x3,x4
     labelimage.destroy()
+    labelresulttext.destroy()
+    quitbutton.destroy()
+    bonus.destroy()
+    bonimage = Label(root,background = "#ffffff")
+    bonimage.pack(pady=(50,30))
+    boques = PhotoImage(file = "giphy.gif")
+    bonimage.configure(image = boques)
+    bonimage.image = boques
+    bontext = Label(root,text = "Which famous Hollywood\nmovie is this scene from? ",fg = "#2481BE",bg = "#ffffff",font = ("Consolas",25))
+    bontext.pack()
 
+    global radiobonus
+    radiobonus = IntVar()
+    radiobonus.set(-1)
 
+    x1 = Radiobutton(root,text = "Pulp Fiction",font = ("Times",20),value = 0,variable = radiobonus,background = "#ffffff",height = 2,width = 25,foreground = "#54BAD8",command = wrong)
+    x1.pack(pady = (15,25))
+
+    x2 = Radiobutton(root,text = "The Shawshank Redemption",font = ("Times",20),value = 1,variable = radiobonus,background = "#ffffff",height = 2,width = 25,foreground = "#54BAD8",command = correct)
+    x2.pack(pady = (15,25))
+    
+    x3 = Radiobutton(root,text = "The Godfather I",font = ("Times",20),value = 2,variable = radiobonus,background = "#ffffff",height = 2,width = 25,foreground = "#54BAD8",command = wrong)
+    x3.pack(pady = (15,25))
+    
+    x4 = Radiobutton(root,text = "The Matrix",font = ("Times",20),value = 3,variable = radiobonus,background = "#ffffff",height = 2,width = 25,foreground = "#54BAD8",command = wrong)
+    x4.pack(pady = (15,25))
 
 
 def showresult(score):
+    global user
+    global labelimage
+    global labelresulttext
+    global quitbutton
+    global bonus
     lblquestion.destroy()
     r1.destroy()
     r2.destroy()
@@ -58,15 +136,19 @@ def showresult(score):
     labelimage.pack(pady=(150,30))
     labelresulttext = Label(root,font = ("Consolas",30),background = "#ffffff")
     labelresulttext.pack()
-    bonus = Button(root,text="Bonus",fg = "blue",height = 2,width=7,background = "#ffffff",font = ("Comic sans MS",30),command = bonques)
-    bonus.pack(pady = (15,30))
-    if score >= 15:
-        image = PhotoImage(file = "jk2.gif")
+    quitbutton = Button(root,text="Quit",fg = "blue",height = 2,width=7,background = "#ffffff",font = ("Comic sans MS",30),command = quit)
+    quitbutton.pack(pady = (15,30))
+    bonus = Frame(root)
+    bonus.pack(pady=(5,20))
+    Button(bonus,text="Bonus",fg = "green",height = 2,width=7,background = "#ffffff",font = ("Comic sans MS",30),command = bonround).pack()
+
+    if score >= (user*5)//2:
+        image = PhotoImage(file = "download.png")
         labelimage.configure(image = image)
         labelimage.image = image
-        labelresulttext.configure(text = "Nice Work!\nYour score is:{}".format(score))
+        labelresulttext.configure(text = "Nice Work!\nYour score is:{}".format(score),foreground = "#E77471")
     else:
-        image = PhotoImage(file = "rm.gif")
+        image = PhotoImage(file = "bear.gif")
         labelimage.configure(image = image)
         labelimage.image = image
         labelresulttext.configure(text = "You can do better\nYour score is: {}".format(score))
@@ -76,6 +158,7 @@ def showresult(score):
 
 
 def calc():
+    global score
     global indexes,user_answer,answers
     x = 0
     score = 0
@@ -91,13 +174,14 @@ def calc():
 
 ques = 1
 def selected():
+    global user
     global radiovar,user_answer
     global lblquestion,r1,r2,r3,r4
     global ques
     x = radiovar.get()
     user_answer.append(x)
     radiovar.set(-1)
-    if ques<5:
+    if ques<user:
         lblquestion.config(text = questions[indexes[ques]])
         r1["text"] = answers_choice[indexes[ques]][0]
         r2["text"] = answers_choice[indexes[ques]][1]
@@ -111,10 +195,12 @@ def selected():
 
 
 
+def startquiz(): # Question and MC options
+    int_text.destroy()
+    int_box.destroy()
+    int_button.destroy()
+    int_answer.destroy()
 
-
-
-def startquiz():
     global lblquestion,r1,r2,r3,r4
     lblquestion = Label(root,text = questions[indexes[0]],font = ("Consolas",25),width = 300,justify = "center",wraplength = 400,background = "#ffffff",foreground = "#FC6C85")
     lblquestion.pack(pady = (30,50))
@@ -134,16 +220,53 @@ def startquiz():
     
     r4 = Radiobutton(root,text = answers_choice[indexes[0]][3],font = ("Times",20),value = 3,variable = radiovar,background = "#ffffff",height = 2,width = 25,foreground = "#E77471",command = selected,)
     r4.pack(pady = (30,50))
-    
+
+
+
+
+
+def number():
+    global user
+    try:
+        user = int(int_box.get())
+    except ValueError:
+        int_answer.config(text = "Please enter a number between 1-10",bg = "#ffffff",fg = "#C42121")
+    if user <= 10 and user >= 1:
+        gen()
+        startquiz()
+    else:
+        int_answer.config(text = "Please enter a number between 1-10",bg = "#ffffff",fg = "#C42121")
+
+    # int_text.destroy()
+    # int_box.destroy()
+    # int_button.destroy()
+    # gen()
+    # startquiz()
 
 def startispressed(): # Start button command
+    global int_text
+    global int_box
+    global int_button
+    global int_answer
     labelimage.destroy()
     labeltext.destroy()
     instruction.destroy()
     rules.destroy()  
     fm.destroy()
-    gen()
-    startquiz()
+    
+    int_text = Label(root,text = "How many questions would you like?",fg = "#2481BE",bg = "#ffffff",font = ("Consolas",25),width = 40,height = 3)
+    int_text.pack()
+    int_box = Entry(root)
+    int_box.pack(pady = 20)
+    int_button = Button(root,text = "Start",height = 2,width = 10,command = number,bg = "#ffffff",fg = "#1B9324")
+    int_button.pack(pady = (10,20))
+    int_answer = Label(root)
+    int_answer.pack(pady = (20,26))
+
+
+
+    # gen()
+    # startquiz()
 
 
 # Starting point
@@ -158,7 +281,7 @@ labelimage = Label(root,image = image1,background = "#ffffff",height = 400,width
 labelimage.pack()
 
 # Quiz Title
-labeltext = Label(root,text = "Title",font = ("Comic sans MS",44),background = "#ffffff")
+labeltext = Label(root,text = "Quiz Master",font = ("Comic sans MS",44),background = "#ffffff")
 labeltext.pack(pady=(5,20))
 
 # Start Button
