0e6181f3a00743680140e56d7e2c5b709113effb
Harsimran Thethi <harsimran.thethi@gmail.com>
2021-03-11 15:47:31 +0530

    final

--- Quiz_gui.py
+++ Quiz_gui.py
@@ -3,41 +3,72 @@ from tkinter import *
 import random
 
 questions  = ["The place of origin of an earthquake is called:",
-"Who is the english physicist responsible for the Big Bang Theory?",
-"The busiest  sea route is:",
-"This is a sample ques 4",
-"This is a sample ques 5",
-"This is a sample ques 6",
-"This is a sample ques 7",
-"This is a sample ques 8",
-"This is a sample ques 9",
-"This is a sample ques 10"]
+"Who is the English Physicist responsible for the Big Bang Theory?",
+"Which is the deepest lake in the world? ",
+"Who is known as the 'Nightingale of India?",
+"Which of the following has no skeleton at all?",
+"Where is the headquarters of SAARC?",
+"Who was the first president of Russia?",
+"Which two calender months are named after Roman Emperors?",
+"The world's most populated country is:",
+"The capital of Philippines is:",
+"The Great Barrier Reef is off the coast of which country?",
+"Which is the world's smallest country?",
+"In which country is Mount Fuji located?",
+"Which of these cities is not a national capital?",
+"Which was the first newspaper of India?",
+"Who was the Prime Minister of Chandragupta Maurya?",
+"In which year was the state of Mysore renamed as Karnataka?",
+"Which of the following is a 'classical' dance form?",
+"Dr.MS.Subbalakshmi has distinguished herself in the field of?",
+"The correct abbreviation of 'BEL' is:",
+"What was Pantone's Colour of the Year 2019?",
+"Rama is an Avathara of which God?",
+"Which of these instruments has the most number of strings?",
+"Who is the writer of Ramayana?",
+"In which year was the first Oscar Award presented? "]
 
 
 
 answers_choice = [
 ["Epicentre","Seismal","Focus","Amphidromic Point"],
 ["Albert Einstein","Michael Skube","George Gamow","Roger Penrose"],
-["The Mediterranean Red-Sea Route","The South Atlantic Route","The North Atlantic Route","The Pacific Route"],
-["13","14","15","16"],
-["17","18","19","20"],
-["21","22","23","24"],
-["25","26","27","28"],
-["29","30","31","32"],
-["33","34","35","36"],
-["37","38","39","40"]
+["Crater Lake","Lake Baikal ","Lake Ysyk","Caspian Sea "],
+["Indira Gandhi","Kittur Rani Chennamma","Savithri Bai Phule","Sarojini Naidu"],
+["Starfish","Sponge","Jellyfish","Silver Fish"],
+["Dhaka","Jakarta","Kathmandu","Manila"],
+["Boris Yeltsin","Vladimir Putin","Dmitry Medvedev","Kokichi"],
+["July and June","December and May","July and August","March and April"],
+["USA","Russia","India","China"],
+["Dili","Manila","Marawi","Jakarta"],
+["Fiji","Australia","New Zealand","South Africa"],
+["Monaco","Lichtenstein","Vatican City","Luxembourg"],
+["China","Peru","North Korea","Japan"],
+["Sydney","Prague","Cairo","Bangkok"],
+["The Hindu","Bengal Gazette","Amrit Bazar Patrika","The Bombay Chronicle"],
+["Saktar","Birbal","Kautilya","Chanakya"],
+["1947","1960","1970","1956"],
+["Kalaripayattu","Kathakali","Chhobia","Bhawai"],
+["Kathak","Bharath Natyam","Playing Violin","Vocal Music"],
+["Bharat Electronics Limited","Bangalore Electronics Limited","Belagaum Electronics Limited","Bellary Electronics Limited"],
+["Rose Quartz","Turquoise","Living Coral","Sand Dollar"],
+["Brahma","Indra","Shiva","Vishnu"],
+["Guitar","Ukulele","Violin","Sitar"],
+["Valmiki","Ved Vyas","Shri Krishna","Rama"],
+["1967","1929","1983","1934"]
 ]
 
-answers = [2,2,2,1,1,1,1,1,1,1]
+answers = [2,2,1,3,2,2,0,2,3,1,1,2,3,0,1,3,3,1,3,0,2,3,0,0,1]
 answers_choice_bonus = [1]
 user_answer = []
 user_answerb = []
 indexes = []
-def gen():
+
+def gen(): # Randomizing Questions
     global user
     global indexes
     while(len(indexes)<user):
-        x = random.randint(0,9)
+        x = random.randint(0,24)
         if x in indexes:
             continue
         else:
@@ -48,7 +79,7 @@ def quit(): # Quit
 
 
 
-def wrong():
+def wrong(): # Wrong Answer Graphics
     bonimage.destroy()
     bontext.destroy()
     x1.destroy()
@@ -62,11 +93,11 @@ def wrong():
     bonus_image.image = bonus_ques
     labelresulttext_bonus = Label(root,font = ("Consolas",30),background = "#ffffff")
     labelresulttext_bonus.pack()
-    labelresulttext_bonus.configure(text = "Wrong Answer\nYour score is:{}".format(score),foreground = "#E77471")
+    labelresulttext_bonus.configure(text = "Wrong Answer\nYour final score is:{}\nThanks for playing!".format(score),foreground = "#E77471")
     quitwrong = Button(root,text="Quit",fg = "blue",height = 2,width=7,background = "#ffffff",font = ("Comic sans MS",30),command = quit)
     quitwrong.pack(pady = (15,30))
 
-def correct():
+def correct(): # Correct Answer Graphics
     global score
     bonimage.destroy()
     bontext.destroy()
@@ -82,7 +113,7 @@ def correct():
     labelresulttext_bonus = Label(root,font = ("Consolas",30),background = "#ffffff")
     labelresulttext_bonus.pack()
     score = score + 10
-    labelresulttext_bonus.configure(text = "Correct!\nYour score is:{}\nThanks for playing!".format(score),foreground = "#E77471")
+    labelresulttext_bonus.configure(text = "Correct!\nYou get 10 points\nYour final score is:{}\nThanks for playing!".format(score),foreground = "#E77471")
     quitcorrect = Button(root,text="Quit",fg = "#ffffff",height = 2,width=7,background = "#FF0000",font = ("Comic sans MS",30),command = quit)
     quitcorrect.pack(pady = (15,30))
 
@@ -101,7 +132,7 @@ def bonround(): #Bonus question
     boques = PhotoImage(file = "giphy.gif")
     bonimage.configure(image = boques)
     bonimage.image = boques
-    bontext = Label(root,text = "Which famous Hollywood\nmovie is this scene from? ",fg = "#2481BE",bg = "#ffffff",font = ("Consolas",25))
+    bontext = Label(root,text = "Which famous Hollywood\nmovie is this scene from? ",fg = "#2481BE",bg = "#C5FC99",font = ("Consolas",25))
     bontext.pack()
 
     global radiobonus
@@ -121,7 +152,7 @@ def bonround(): #Bonus question
     x4.pack(pady = (15,25))
 
 
-def showresult(score):
+def showresult(score): # Results
     global user
     global labelimage
     global labelresulttext
@@ -140,24 +171,24 @@ def showresult(score):
     quitbutton.pack(pady = (15,30))
     bonus = Frame(root)
     bonus.pack(pady=(5,20))
-    Button(bonus,text="Bonus",fg = "C68E17",height = 2,width=7,background = "#FFFF00",font = ("Comic sans MS",30),command = bonround).pack()
+    Button(bonus,text="Bonus",fg = "#C68E17",height = 2,width=7,background = "#FFFF00",font = ("Comic sans MS",30),command = bonround).pack()
 
     if score >= (user*5)//2:
         image = PhotoImage(file = "gif.gif")
         labelimage.configure(image = image)
         labelimage.image = image
-        labelresulttext.configure(text = "Nice Work!\nYour score is:{}".format(score),foreground = "#E77471")
+        labelresulttext.configure(text = "Nice Work!\nYour score is:{}/{}".format(score,user*5),foreground = "#E77471",bg = "#C5FC99")
     else:
-        image = PhotoImage(file = "meme.png")
+        image = PhotoImage(file = "obama.gif")
         labelimage.configure(image = image)
         labelimage.image = image
-        labelresulttext.configure(text = "You can do better\nYour score is: {}".format(score))
+        labelresulttext.configure(text = "You can do better\nYour score is: {}/{}".format(score,user*5),foreground = "#E77471",bg = "#C5FC99")
 
 
 
 
 
-def calc():
+def calc(): # Score calculation
     global score
     global indexes,user_answer,answers
     x = 0
@@ -173,7 +204,7 @@ def calc():
 
 
 ques = 1
-def selected():
+def selected(): # Selecting Options(No graphics)
     global user
     global radiovar,user_answer
     global lblquestion,r1,r2,r3,r4
@@ -195,7 +226,7 @@ def selected():
 
 
 
-def startquiz(): # Question and MC options
+def startquiz(): # Question and MC Options
     int_text.destroy()
     int_box.destroy()
     int_button.destroy()
@@ -225,17 +256,17 @@ def startquiz(): # Question and MC options
 
 
 
-def number():
+def number(): # Number Error Message
     global user
     try:
         user = int(int_box.get())
     except ValueError:
-        int_answer.config(text = "Please enter a number between 1-10",bg = "#ffffff",fg = "#C42121")
-    if user <= 10 and user >= 1:
+        int_answer.config(text = "Please enter a number between 1-25",bg = "#C5FC99",fg = "#C42121")
+    if user <= 25 and user >= 1:
         gen()
         startquiz()
     else:
-        int_answer.config(text = "Please enter a number between 1-10",bg = "#ffffff",fg = "#C42121")
+        int_answer.config(text = "Please enter a number between 1-25",bg = "#C5FC99",fg = "#C42121")
 
     # int_text.destroy()
     # int_box.destroy()
@@ -254,7 +285,7 @@ def startispressed(): # Start button command
     rules.destroy()  
     fm.destroy()
     
-    int_text = Label(root,text = "How many questions would you like?",fg = "#254117",bg = "#48CCCD",font = ("Consolas",25),width = 40,height = 3)
+    int_text = Label(root,text = "How many questions would you like to answer?",fg = "#254117",bg = "#48CCCD",font = ("Consolas",25),width = 50,height = 3)
     int_text.pack()
     int_box = Entry(root)
     int_box.pack(pady = 20)
@@ -264,39 +295,34 @@ def startispressed(): # Start button command
     int_answer.pack(pady = (20,26))
 
 
-
-    # gen()
-    # startquiz()
-
-
 # Starting point
 root = tkinter.Tk()
 root.title("Quiz Master")
-root.geometry("1200x900")
-root.config(background = "#b1fb17")
+root.geometry("1300x900")
+root.config(background = "#C5FC99")
 
 # First crown image
 image1 = PhotoImage(file="crown2.gif")
-labelimage = Label(root,image = image1,background = "#ffffff",height = 400,width = 600)
+labelimage = Label(root,image = image1,background = "#C5FC99",height = 400,width = 600)
 labelimage.pack()
 
 # Quiz Title
-labeltext = Label(root,text = "Quiz Master",font = ("Comic sans MS",44),background = "#b1fb17")
+labeltext = Label(root,text = "Quiz Master",font = ("Comic sans MS",44),background = "#C5FC99")
 labeltext.pack(pady=(5,20))
 
 # Start Button
 fm = Frame(root)
 fm.pack(pady=(5,20))
-Button(fm,text="Start",fg = "ffffff",height = 2,width=7,background = "#ffff00",font = ("Comic sans MS",30),command = startispressed).pack()
+Button(fm,text="Start",fg = "#ffffff",height = 2,width=7,background = "#ffff00",font = ("Comic sans MS",30),command = startispressed).pack()
 
 # Instructions and rules
-instruction = Label(root,text = "Read the rules\nClick once you are ready",background = "#ffffff",font = ("Consolas",14),justify = "center")
+instruction = Label(root,text = "Read the rules\nClick once you are ready",background = "#C5FC99",font = ("Consolas",14),justify = "center")
 instruction.pack(pady=(10,40))
-rules = Label(root,text = """The quiz consists of 5 questions.Each question in the quiz is of multiple-choice format.
- Read each question carefully,and select a suitable answer for each of the questions. The questions in the quiz are based on general subjects.
-  Each correct or incorrect response will result in an appropriate feedback immediately on screen.
-The entered option will be the final and cannot be changed. The total score for the quiz is based on your responses to all questions.
-In the end of the quiz, you will be provided with a choice to answer a bonus question. """,width=160,height=15,font = ("Times",15),background = "#fff8c6",foreground = "#000000")
+rules = Label(root,text = """Please enter the number of questions you would like to answer at the beginning of the quiz. Each question in the quiz is of multiple-choice format.
+The questions in the quiz are based on general subjects. Read the question carefully,and select a suitable answer for each of the questions. 
+Each correct response is worth 5 points.
+Warning: The entered option will be the final and cannot be changed. The total score for the quiz is based on your responses to all questions.
+At the end of the quiz, you will be provided with a choice to answer a bonus question worth 10 points. Good luck! """,width=190,height=12,font = ("Times",16),background = "#fff8c6",foreground = "#000000")
 rules.pack()
 
 root.mainloop() # To run
